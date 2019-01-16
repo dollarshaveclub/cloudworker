700cf4cacde2648b1f77fa36bde99c193e58c79c
Hank Jacobs <hank8903@gmail.com>
2019-01-16 10:12:44 -0800

    Export portion of runtime that can be used in a Worker script

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const runtime = require('./lib/runtime')
+
 module.exports = require('./lib/cloudworker')
-module.exports.Request = require('./lib/runtime').Request
-module.exports.Response = require('./lib/runtime').Response
+
+module.exports.fetch = runtime.fetch
+module.exports.FetchEvent = runtime.FetchEvent
+module.exports.URL = runtime.URL
+module.exports.Headers = runtime.Headers
+module.exports.Request = runtime.Request
+module.exports.Response = runtime.Response
+module.exports.ReadableStream = runtime.ReadableStream
+module.exports.WritableStream = runtime.WritableStream
+module.exports.TransformStream = runtime.TransformStream
+module.exports.TextDecoder = runtime.TextDecoder
+module.exports.TextEncoder = runtime.TextEncoder
+module.exports.atob = runtime.atob
+module.exports.btoa = runtime.btoa

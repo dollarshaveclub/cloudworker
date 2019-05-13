a417b7b590c30f73240cdca98163d84714e0de08
Mario Villaplana <mario.villaplana@gmail.com>
2019-05-13 17:53:07 -0500

    Add TLS support
    This adds support for processing HTTPS requests on a separate port from
    the usual HTTP requests. It does so by adding options for specifying the
    path to a TLS key and a TLS certificate.
    
    This can be useful for testing worker scripts that do things like custom
    redirects depending on whether the user is making an encrypted request
    or not.
    
    Note: This PR does not yet set special `request.cf` attributes such as
    `tlsVersion` and `tlsCipher` as described here:
    https://developers.cloudflare.com/workers/reference/request-attributes/

--- bin/cloudworker.js
+++ bin/cloudworker.js
@@ -24,6 +24,9 @@ program
   .option('-c, --enable-cache', 'Enables cache <BETA>', false)
   .option('-r, --watch', 'Watch the worker script and restart the worker when changes are detected', false)
   .option('-s, --set [variable.key=value]', '(Deprecated) Binds variable to a local implementation of Workers KV and sets key to value', collect, [])
+  .option('--tls-key <tlsKey>', 'Optional. Path to encryption key for serving requests with TLS enabled. Must specify --tls-cert when using this option.')
+  .option('--tls-cert <tlsCert>', 'Optional. Path to certificate for serving requests with TLS enabled. Must specify --tls-key when using this option.')
+  .option('--https-port <httpsPort>', 'Optional. Port to listen on for HTTPS requests. Must specify --tls-cert and --tls-key when using this option. May not be the same value as --port.', 3001)
   .action(f => { file = f })
   .parse(process.argv)
 
@@ -50,10 +53,44 @@ function run (file, wasmBindings) {
   // Add a warning log for deprecation
   if (program.set.length > 0) console.warn('Warning: Flag --set is now deprecated, please use --kv-set instead')
 
-  const opts = {debug: program.debug, enableCache: program.enableCache, bindings: bindings}
-  let server = new Cloudworker(script, opts).listen(program.port)
+  if ((program.tlsKey && !program.tlsCert) || (!program.tlsKey && program.tlsCert)) {
+    console.error('Both --tls-key and --tls-cert must be set when using TLS.')
+    process.exit(1)
+  }
 
-  console.log(`Listening on ${program.port}`)
+  let tlsKey = ''
+  let tlsCert = ''
+  if (program.tlsKey && program.tlsCert) {
+    try {
+      tlsKey = fs.readFileSync(program.tlsKey)
+      tlsCert = fs.readFileSync(program.tlsCert)
+    } catch (err) {
+      console.error('Error reading TLS configuration')
+      console.error(err)
+      process.exit(1)
+    }
+    if (program.port === program.httpsPort) {
+      console.error('HTTP port and HTTPS port must be different')
+      process.exit(1)
+    }
+  }
+
+  const opts = {
+    debug: program.debug,
+    enableCache: program.enableCache,
+    bindings: bindings,
+    tlsKey: tlsKey,
+    tlsCert: tlsCert,
+  }
+  let worker = new Cloudworker(script, opts)
+  let server = worker.listen(program.port)
+  console.log(`Listening on ${program.port} for HTTP requests`)
+
+  let httpsServer = null
+  if (tlsKey && tlsCert) {
+    httpsServer = worker.httpsListen(program.httpsPort)
+    console.log(`Listening on ${program.httpsPort} for HTTPS requests`)
+  }
 
   let stopping = false
   let reloading = false
@@ -64,12 +101,23 @@ function run (file, wasmBindings) {
       console.log('Changes to the worker script detected - reloading...')
 
       server.close(() => {
-        if (stopping) return
+        if (stopping) {
+          if (httpsServer) {
+            httpsServer.close(() => { })
+          }
+          return
+        }
 
+        worker = new Cloudworker(utils.read(fullpath), opts)
+        server = worker.listen(program.port)
+
+        if (httpsServer) {
+          httpsServer.close(() => {
+            httpsServer = worker.httpsListen(program.httpsPort)
+          })
+        }
         reloading = false
-        console.log('Successfully reloaded!')
-
-        server = new Cloudworker(utils.read(fullpath), opts).listen(program.port)
+        console.log('Successfully reloaded server!')
       })
     })
   }
@@ -80,8 +128,16 @@ function run (file, wasmBindings) {
     stopping = true
     console.log('\nShutting down...')
     server.close(terminate)
+    if (httpsServer) {
+      httpsServer.close(terminate)
+    }
 
-    if (reloading) server.on('close', terminate)
+    if (reloading) {
+      server.on('close', terminate)
+      if (httpsServer) {
+        httpsServer.on('close', terminate)
+      }
+    }
   }
 
   function terminate () {
